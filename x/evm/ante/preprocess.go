cd17946b5a63244b0178a307a3b1b7224521bd15
Jonathan71240 <Jonathan71240@proton.me>
2025-06-18 12:04:45 +0800

    fix gas used err

--- x/evm/ante/preprocess.go
+++ x/evm/ante/preprocess.go
@@ -82,7 +82,7 @@ func (p *EVMPreprocessDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate
 	}
 
 	//// use infinite gas meter for EVM transaction because EVM handles gas checking from within
-	//ctx = ctx.WithGasMeter(storetypes.NewInfiniteGasMeter())
+	ctx = ctx.WithGasMeter(storetypes.NewInfiniteGasMeter())
 	//
 	//derived := msg.Derived
 	//eniAddr := derived.SenderEniAddr
